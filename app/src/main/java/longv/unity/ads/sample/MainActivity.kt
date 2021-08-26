19ac06d42b7bae40890da55bba3ce1a5814e99d6
Long Vu <longvu@longvu-1ulm.lan>
2021-08-26 15:27:41 +0300

    Add banner ad

--- app/src/main/java/longv/unity/ads/sample/MainActivity.kt
+++ app/src/main/java/longv/unity/ads/sample/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import com.unity3d.ads.IUnityAdsInitializationListener
 import com.unity3d.ads.UnityAds
+import com.unity3d.services.banners.BannerErrorInfo
+import com.unity3d.services.banners.BannerView
+import com.unity3d.services.banners.UnityBannerSize
 import longv.unity.ads.sample.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), IUnityAdsInitializationListener {
+class MainActivity : AppCompatActivity(), IUnityAdsInitializationListener, BannerView.IListener {
 
     private lateinit var viewBinding: ActivityMainBinding
 
@@ -17,6 +20,13 @@ class MainActivity : AppCompatActivity(), IUnityAdsInitializationListener {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
+        setupVideoAd()
+        setupBannerAd()
+
+        UnityAds.initialize(applicationContext, "14851", true, this)
+    }
+
+    private fun setupVideoAd() {
         viewBinding.watchAdBtn.text = "Initializing Ads..."
         viewBinding.watchAdBtn.isEnabled = false
         viewBinding.watchAdBtn.setOnClickListener {
@@ -24,8 +34,14 @@ class MainActivity : AppCompatActivity(), IUnityAdsInitializationListener {
                 UnityAds.show(this)
             }
         }
+    }
 
-        UnityAds.initialize(applicationContext, "14851", this)
+    private fun setupBannerAd() {
+       BannerView(this, "bannerads", UnityBannerSize(320, 50)).apply {
+           viewBinding.bannerAdContainer.addView(this)
+           listener = this@MainActivity
+           load()
+       }
     }
 
     override fun onInitializationComplete() {
@@ -33,8 +49,24 @@ class MainActivity : AppCompatActivity(), IUnityAdsInitializationListener {
         viewBinding.watchAdBtn.isEnabled = true
     }
 
-    override fun onInitializationFailed(error: UnityAds.UnityAdsInitializationError?, message: String?) {
+    override fun onInitializationFailed(error: UnityAds.UnityAdsInitializationError, message: String) {
         Snackbar.make(viewBinding.root, "Fail to initialize Unity Ads: $message", Snackbar.LENGTH_SHORT)
+            .show()
+    }
+
+    override fun onBannerLoaded(view: BannerView) {
+    }
+
+    override fun onBannerClick(view: BannerView) {
+    }
+
+    override fun onBannerFailedToLoad(view: BannerView, info: BannerErrorInfo) {
+        Snackbar.make(viewBinding.root, "Fail to load banner ad: $info", Snackbar.LENGTH_SHORT)
+            .show()
+    }
+
+    override fun onBannerLeftApplication(view: BannerView?) {
+        Snackbar.make(viewBinding.root, "Banner has left the app", Snackbar.LENGTH_SHORT)
             .show()
     }
 }
